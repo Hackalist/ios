486af8b6644ba94639ef3060bb3cffe801548b3a
Sergheev Andrian <sergheev@icloud.com>
2018-06-22 11:31:46 +0300

    Initial Commit

---
+++ Hackalist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackalist
+//
+//  Created by Sergheev Andrian on 6/22/18.
+//  Copyright © 2018 Sergheev Andrian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
